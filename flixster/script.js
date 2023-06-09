9cd457bf0901ebb359ac5cecc2db5697d8819764
Bravo-Luis <luisbravo@ucsb.edu>
2023-06-09 07:09:24 -0700

    idk

--- flixster/script.js
+++ flixster/script.js
@@ -1,12 +1,11 @@
 
 // HTML REFERENCES
-const movieContainer = document.querySelector("#movie-list")
+const moviesContainer = document.querySelector("#movie-list")
 const loadMoreButton = document.querySelector("#load-more-button")
 const movieBoxes = document.querySelectorAll('.movie-container')
-const url = 'https://api.themoviedb.org/3/discover/movie?api_key=490dc857a12acaf336b38aca7b1fea9a'
-let movieCollection = []
 
 class movie{
+
     constructor(title, imgPath, rating){
         this.title = title
         this.rating = rating
@@ -14,49 +13,111 @@ class movie{
     }
 
     toHTML(){
-        return '<div class=\"movie-container\"> <img class="movie-img" src=\" ' + 'https://image.tmdb.org/t/p/w342' + this.imgPath + '\"/> <br>' + '⭐️ ' + this.rating + '<br>' + this.title +' </div>'
+        return `<div class="movie-container"> 
+                    <img class="movie-img" src="https://image.tmdb.org/t/p/w342${this.imgPath}"/> 
+                    <br> ⭐️ ${this.rating} 
+                    <br> ${this.title} 
+                </div>`
     }
+
 }
 
-async function APICall(){
-    const response = await fetch(url);
-    const data = await response.json();
-    JSONConvert(data);
-}
+class movieManager{
 
-function JSONConvert(input){
-    for (let i= 0; i<input.results.length; i++){
-        movieCollection.push(new movie(input.results[i].title, input.results[i].poster_path, input.results[i].vote_average))
+    constructor(){
+        this.movies = []
+        this.newMovies = []
+        this.currentPage = 1
     }
-}
 
-let input = document.querySelector("#load-more-button");
-input.addEventListener("click", (event) => {
-  event.preventDefault();
-  for(let i = 0; i < 5; i++){
-    try{
-        movieContainer.innerHTML += movieCollection[i].toHTML()
-        movieCollection.splice(i, 1)
-
-        if (movieCollection.length == 0) {
-            input.style.display = "none"
+    async fetchMovies(){
+        let url = `https://api.themoviedb.org/3/movie/now_playing?page=${this.currentPage}&api_key=490dc857a12acaf336b38aca7b1fea9a`
+        const response = await fetch(url)
+        const input = await response.json()
+        for (let i= 0; i<input.results.length; i++){
+            this.newMovies.push(new movie(input.results[i].title, input.results[i].poster_path, input.results[i].vote_average))
         }
+        this.currentPage++
+    }
 
-    } catch {
+    populateHTML(){
+        for(let i = 0; i < this.newMovies.length; i++){
+            this.movies.push(this.newMovies[i])
+        }
+        this.newMovies = []
+    }
 
+    async initialPopulate(){
+        await this.fetchMovies()
+        this.populateHTML()
+        for(let i = 0; i < this.movies.length; i++){
+            moviesContainer.innerHTML += this.movies[i].toHTML()
+        }
+    }    
+
+    search(searchFieldText){
+        moviesContainer.innerHTML = ""
+        for(let i = 0; i < this.movies.length; i++){
+            if (this.movies[i].title.substring(0, searchFieldText.length).toLowerCase() == searchFieldText.toLowerCase()){
+                moviesContainer.innerHTML += this.movies[i].toHTML()
+            }
+        }
     }
 }
+
+
+
+let movManager = new movieManager()
+window.onload = async function () {
+    await movManager.initialPopulate()
+}
+
+input = document.querySelector("#load-more-button")
+input.addEventListener("click", async (event) => {
+    event.preventDefault()
+    let currentSearch = document.querySelector('#search-input').value
+
+    if (!currentSearch) {
+        await movManager.fetchMovies()
+        movManager.populateHTML()
+        movManager.search(currentSearch)
+        return
+    }
+
+    let attempts = 0
+    const maxAttempts = 10
+    let foundMatchingMovie = false
+
+    while (!foundMatchingMovie && attempts < maxAttempts) {
+        attempts++
+        await movManager.fetchMovies()
+        movManager.populateHTML()
+        movManager.search(currentSearch)
+
+        for (let mov of movManager.newMovies) {
+            if (mov.title.substring(0, currentSearch.length).toLowerCase() == currentSearch.toLowerCase()) {
+                foundMatchingMovie = true
+                break
+            }
+        }
+    }
+    
+    if (!foundMatchingMovie){
+        document.querySelector('#no-matches-message').style.display = ""
+        input.style.display = "none"
+    }
 })
 
 
 
+document.querySelector('#search-input').addEventListener("input", function(event) {
+    event.preventDefault()
+    let searchFieldText = event.target.value=
+    movManager.search(searchFieldText)
+    input.style.display = ""
+    document.querySelector('#no-matches-message').style.display = "none"
+})
 
-
-window.onload = async function () {
-    await APICall();
-    for(let i = 0; i < 5; i++){
-        movieContainer.innerHTML += movieCollection[i].toHTML()
-        movieCollection.splice(i, 1)
-        
-    }
-}
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+})
