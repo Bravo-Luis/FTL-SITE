3b3829fe943e02f5753309b48bd110ba15233268
Bravo-Luis <luisbravo@ucsb.edu>
2023-06-09 10:34:13 -0700

    good

--- flixster/script.js
+++ flixster/script.js
@@ -27,24 +27,39 @@ class movieManager{
     constructor(){
         this.movies = []
         this.newMovies = []
+        this.searchMovies= []
         this.currentPage = 1
     }
 
-    async fetchMovies(){
-        let url = `https://api.themoviedb.org/3/movie/now_playing?page=${this.currentPage}&api_key=490dc857a12acaf336b38aca7b1fea9a`
+    async fetchMovies(search=false, query=""){
+        let url;
+        if (!search){
+            url = `https://api.themoviedb.org/3/movie/now_playing?page=${this.currentPage}&api_key=490dc857a12acaf336b38aca7b1fea9a`
+        }
+        else{
+            url = `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=490dc857a12acaf336b38aca7b1fea9a`
+        }
         const response = await fetch(url)
         const input = await response.json()
+        this.searchMovies = []
         for (let i= 0; i<input.results.length; i++){
-            this.newMovies.push(new movie(input.results[i].title, input.results[i].poster_path, input.results[i].vote_average))
+            if (!search){this.newMovies.push(new movie(input.results[i].title, input.results[i].poster_path, input.results[i].vote_average))}
+            else {this.searchMovies.push(new movie(input.results[i].title, input.results[i].poster_path, input.results[i].vote_average))}
         }
-        this.currentPage++
+        if (!search){this.currentPage++}
+        else {}
     }
 
-    populateHTML(){
-        for(let i = 0; i < this.newMovies.length; i++){
-            this.movies.push(this.newMovies[i])
+    populateHTML(search=false){
+        if(!search){
+            for(let i = 0; i < this.newMovies.length; i++){
+                this.movies.push(this.newMovies[i])
+            }
+            this.newMovies = []
         }
-        this.newMovies = []
+        else{
+
+        }
     }
 
     async initialPopulate(){
@@ -55,17 +70,14 @@ class movieManager{
         }
     }    
 
-    search(searchFieldText){
+    async search(searchFieldText){
         moviesContainer.innerHTML = ""
-        for(let i = 0; i < this.movies.length; i++){
-            if (this.movies[i].title.substring(0, searchFieldText.length).toLowerCase() == searchFieldText.toLowerCase()){
-                moviesContainer.innerHTML += this.movies[i].toHTML()
-            }
+        await this.fetchMovies(true, searchFieldText)
+        for(let i = 0; i < this.searchMovies.length; i++){
+            moviesContainer.innerHTML += this.searchMovies[i].toHTML()
         }
     }
 }
-
-
 
 let movManager = new movieManager()
 window.onload = async function () {
@@ -75,47 +87,25 @@ window.onload = async function () {
 input = document.querySelector("#load-more-button")
 input.addEventListener("click", async (event) => {
     event.preventDefault()
-    let currentSearch = document.querySelector('#search-input').value
-
-    if (!currentSearch) {
-        await movManager.fetchMovies()
-        movManager.populateHTML()
-        movManager.search(currentSearch)
-        return
-    }
-
-    let attempts = 0
-    const maxAttempts = 10
-    let foundMatchingMovie = false
-
-    while (!foundMatchingMovie && attempts < maxAttempts) {
-        attempts++
-        await movManager.fetchMovies()
-        movManager.populateHTML()
-        movManager.search(currentSearch)
-
-        for (let mov of movManager.newMovies) {
-            if (mov.title.substring(0, currentSearch.length).toLowerCase() == currentSearch.toLowerCase()) {
-                foundMatchingMovie = true
-                break
-            }
-        }
-    }
-    
-    if (!foundMatchingMovie){
-        document.querySelector('#no-matches-message').style.display = ""
-        input.style.display = "none"
-    }
+    await movManager.fetchMovies()
+    movManager.populateHTML()
 })
 
-
+noMatchesMessage = document.querySelector("#no-matches-message")
 
 document.querySelector('#search-input').addEventListener("input", function(event) {
     event.preventDefault()
     let searchFieldText = event.target.value
     movManager.search(searchFieldText)
-    input.style.display = ""
-    document.querySelector('#no-matches-message').style.display = "none"
+
+    if (event.target.value == ""){
+        movManager.currentPage = 1 
+        movManager.initialPopulate()
+        input.style.display = ""
+    }else{
+        input.style.display = "none"
+    }
+
 })
 
 document.querySelector('form').addEventListener('submit', function(event) {
