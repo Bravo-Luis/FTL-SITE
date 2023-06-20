885915ff5cf2e1178917f746ae08db5e5606d71e
Luis Bravo <lbravo@lbravo-ltmxcuw.internal.salesforce.com>
2023-06-20 15:58:10 -0700

    update

--- store/student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ store/student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import "./Sidebar.css";
+import { Link } from "react-router-dom";
 
 export default function Sidebar({ cart, setCart }) {
   const [isOpen, setOpen] = React.useState(false);
@@ -112,7 +113,7 @@ function ShoppingList({ cart, setCart }) {
           item.quantity > 0 && (
             <div key={item.id} className="shopping-list-item">
               <button onClick={() => decrementQuantity(item.id)}>-</button>
-              <span className="item-name">{item.name}</span>
+              <Link className="cart-link" to={"products/" + item.id} ><span className="item-name">{item.name}</span></Link>
               <span className="item-quantity">Qty: {item.quantity}</span>
               <span className="item-price">
                 Price: ${item.price.toFixed(2)}
@@ -121,7 +122,11 @@ function ShoppingList({ cart, setCart }) {
           )
       )}
       {totalPrice > 0 ? (
-        <div className="total-price">Total: ${totalPrice.toFixed(2)}</div>
+        <>
+        <div className="total-price">Subtotal: ${totalPrice.toFixed(2)}</div>
+        <div className="total-price">Taxes: ${(totalPrice * 0.0875).toFixed(2) }</div>
+        <div className="total-price">Total: ${(totalPrice + (totalPrice * 0.0875)).toFixed(2) }</div>
+        </>
       ) : (
         <div>No Items in Shopping List</div>
       )}
