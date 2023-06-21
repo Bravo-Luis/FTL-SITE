29424ae6c4ecec528fdeeca25062da571142d34e
Luis Bravo <lbravo@lbravo-ltmxcuw.internal.salesforce.com>
2023-06-21 10:33:19 -0700

    s

--- store/student-store-express-api/app.js
+++ store/student-store-express-api/app.js
@@ -1,1 +1,22 @@
-// YOUR CODE HERE
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const db = require('./data/db.json')
+
+app.use(cors())
+app.use(express.json())
+
+
+
+app.get('/', (req,res)=>{
+    res.status(200)
+    res.send(db)
+})
+
+
+app.post('/', (req,res)=>{
+    console.log("POST HIT")
+    console.log(req.body)
+})
+
+module.exports = app
